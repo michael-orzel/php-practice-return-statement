dffa38e21d61b80f3d9227f0f38fa4e181247ead
Michael Orzel <56739010+michael-orzel@users.noreply.github.com>
2020-11-05 13:53:41 -0500

    Create site.php

---
+++ site.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            function cube($num)
+            {
+                echo "Nintendo ";
+                return $num * $num * $num;
+            }
+
+            echo cube(4);
+        ?>
+    </body>
+</html>
